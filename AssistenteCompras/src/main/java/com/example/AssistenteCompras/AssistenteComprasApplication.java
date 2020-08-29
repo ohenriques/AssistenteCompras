4849476b240b02c9823ecedabc9e299758c037dd
unknown <rick.souza97@gmail.com>
2020-08-29 10:34:05 -0300

    initial commit

---
+++ AssistenteCompras/src/main/java/com/example/AssistenteCompras/AssistenteComprasApplication.java
@@ -0,0 +1,13 @@
+package com.example.AssistenteCompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenteComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenteComprasApplication.class, args);
+	}
+
+}
---
+++ AssistenteCompras/src/test/java/com/example/AssistenteCompras/AssistenteComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AssistenteCompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenteComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
