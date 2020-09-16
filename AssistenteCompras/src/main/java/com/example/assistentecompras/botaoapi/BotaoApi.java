4749213c97d63d898d6563953f458683d56cee77
Lucas Manzano <lucas@petize.pet>
2020-09-16 08:17:17 -0300

    cria Gondolas e Roteador

--- AssistenteCompras/src/main/java/com/example/assistentecompras/botaoapi/BotaoApi.java
+++ AssistenteCompras/src/main/java/com/example/assistentecompras/botaoapi/BotaoApi.java
@@ -8,11 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BotaoApi {
-    // standard constructors
 	@CrossOrigin
 	@RequestMapping( method = RequestMethod.GET, path="/botaoApi")
     public String getUsers() {
-		System.out.println("LALALAA");
 		return new JSONObject().put("retorno", "Retorno da API").toString();
     }
 }
